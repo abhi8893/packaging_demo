beaec6b84437a787c66bdfef1ce7e5549c867aed
abhi8893 <bhatiaabhishek8893@gmail.com>
2023-12-16 16:27:32 +0530

    first commit

---
+++ run.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+set -e
+
+THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null 2>&1 && pwd )"
+
+function load-dotenv {
+    while read -r line; do
+        export "$line"
+    done < <(grep -v '^#' "$THIS_DIR/.env" | grep -v '^$')
+}
+
+function clean {
+    rm -rf dist build
+
+    find . \
+      -type d \
+      \( \
+        -name "*cache*" \
+        -o -name "*.dist-info" \
+        -o -name "*.egg-info" \
+        -o -name "*htmlcov" \
+      \) \
+      -not -path "*env/*" \
+      -exec rm -r {} + || true
+
+    find . \
+      -type f \
+      -name "*.pyc" \
+      -not -path "*env/*" \
+      -exec rm {} +
+}
+
+function lint {
+    pre-commit run --all-files
+}
+
+function install {
+    pip install --upgrade pip
+    pip install -e "${THIS_DIR}/[dev]"
+}
+
+function build {
+    python -m build --sdist --wheel "${THIS_DIR}/"
+}
+
+function publish:test {
+    load-dotenv
+    twine upload dist/* \
+    --repository testpypi \
+    --username=__token__ \
+    --password="$TEST_PYPI_TOKEN"
+}
+
+function release:test {
+    lint
+    clean
+    build
+    publish:test
+}
+
+function publish:prod {
+    load-dotenv
+    twine upload dist/* \
+    --repository pypi \
+    --username=__token__ \
+    --password="$PROD_PYPI_TOKEN"
+}
+
+function release:prod {
+    release:test
+    publish:prod
+}
+
+
+function help {
+    echo "$0 <task> <args>"
+    echo "Tasks:"
+    compgen -A function | cat -n
+}
+
+TIMEFORMAT="Task completed in %3lR"
+time ${@:-help}
